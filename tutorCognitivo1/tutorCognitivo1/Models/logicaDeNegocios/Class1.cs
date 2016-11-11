190f4a44a5b253886ba06dbe82a66ffc9f1392ed
mariquita5 <majomadrig5@gmail.com>
2016-11-11 15:34:05 -0600

    cambios

---
+++ tutorCognitivo1/tutorCognitivo1/Models/logicaDeNegocios/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using frameworkJava.logicaDeIntegracion;
+
+namespace tutorCognitivo1.Models.logicaDeNegocios
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Traductor traductor = new Traductor();
+            System.Console.WriteLine(traductor.traducirTexto("hola"));
+
+        }
+
+
+    }
+}
---
+++ tutorCognitivo1/tutorCognitivo1/Views/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tutorCognitivo1.Startup))]
+namespace tutorCognitivo1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
