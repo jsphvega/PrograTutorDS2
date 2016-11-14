4837b5108cb6b6b0e82a92b650b4c1362b118ad6
krlaguilar02 <ka_yi_la1502@hotmail.es>
2016-11-14 15:29:53 -0600

    Usuario, Fabrica

--- TutorCognitivoDS2/TutorCognitivoDS2/Modelo/logicaDeNegocios/fabricaUsuario.cs
+++ TutorCognitivoDS2/TutorCognitivoDS2/Modelo/logicaDeNegocios/fabricaUsuario.cs
@@ -1,20 +1,21 @@
 using dto;
 using System;
+using System.Reflection;
 
 namespace logicaDeNegocios
 {
     public class FabricaUsuario
     {
-        /*private String PAQUETE = "frameworkNet.logicaDeNegocio.";
+        private String PAQUETE = "frameworkNet.logicaDeNegocio.";
         private Object usuario;
 
         public void crearUsuario(String pTipoUsuario, DTOUsuario pUsuario)
         {
             Type tipoUsuario = Type.GetType(pTipoUsuario);
 
-            object objeto = Activator.CreateInstance(type1);
-            object[] parametros = new object[] { pUsuario.getNombre(), pUsuario.getApellido() };
-            type1.InvokeMember("registrarUsuario", BindingFlags.InvokeMethod,
+            object objeto = Activator.CreateInstance(tipoUsuario);
+            object[] parametros = new object[] { pUsuario, DateTime.Now };
+            tipoUsuario.InvokeMember("registrarUsuario", BindingFlags.InvokeMethod,
                                                null, objeto, parametros);
 
         }
@@ -22,7 +23,7 @@ namespace logicaDeNegocios
         public void crearUsuario(String pTipoUsuario, DTOUsuario pUsuario, DateTime pVencimiento)
         {
 
-        }*/
+        }
 
     }
 }
