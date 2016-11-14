ace999d4b374d01e8099a907aeae5f8ee222e15c
krlaguilar02 <ka_yi_la1502@hotmail.es>
2016-11-14 14:57:57 -0600

    Clase Tutor[Constructor]

--- TutorCognitivoDS2/TutorCognitivoDS2/Modelo/logicaDeNegocios/Tutor.cs
+++ TutorCognitivoDS2/TutorCognitivoDS2/Modelo/logicaDeNegocios/Tutor.cs
@@ -7,16 +7,20 @@ namespace logicaDeNegocios
     {
         private DateTime vencimiento;
 
-        public Tutor(DTOUsuario pUsuario, DateTime pVencimiento)
-            : base(pUsuario.Nombre, pUsuario.Apellido1, pUsuario.Apellido2, pUsuario.Correo,
-                  pUsuario.Contraseña1, pUsuario.Carrera)
-        {
+        public Tutor(DTOUsuario pUsuario, DateTime pVencimiento) {
 
+            Nombre = pUsuario.Nombre;
+            Apellido1 = pUsuario.Apellido1;
+            Apellido2 = pUsuario.Apellido2;
+            Correo = pUsuario.Correo;
+            Contraseña = pUsuario.Contraseña1;
+            Carrera = pUsuario.Carrera;
         }
 
         public override void registrarUsuario()
         {
             // TODO implement here
+            //SUPONGO QUE AQUI VA EL METODO DE LA BASE DE DATOS
         }
 
         public override String toString()
