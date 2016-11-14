246160b0dd8c3d16f6accf993ddd296eb6bfb83d
krlaguilar02 <ka_yi_la1502@hotmail.es>
2016-11-13 23:08:17 -0600

    CSV

--- TutorCognitivoDS2/TutorCognitivoDS2/Modelo/logicaDeNegocios/CSV.cs
+++ TutorCognitivoDS2/TutorCognitivoDS2/Modelo/logicaDeNegocios/CSV.cs
@@ -2,7 +2,34 @@ namespace logicaDeNegocios
 {
     public class CSV : Bitacora
     {
+        StringWriter sw = new StringWriter();
+        public void crearArchivo()
+        {
+            string attachment = "attachment; filename=BitacoraTutorCognitivo.csv";
 
+            System.Web.HttpContext.Current.Response.Clear();
+            System.Web.HttpContext.Current.Response.ClearHeaders();
+            System.Web.HttpContext.Current.Response.ClearContent();
+            System.Web.HttpContext.Current.Response.AddHeader("content-disposition", attachment);
+            System.Web.HttpContext.Current.Response.ContentType = "text/csv";
+            System.Web.HttpContext.Current.Response.AddHeader("Pragma", "public");
+            escribir();
+        }
+
+
+        public void escribir()
+        {
+
+            sw.WriteLine("\"Usuario\";\"Accion\";\"Fecha y Hora\"");
+
+            sw.WriteLine(string.Format("\"{0}\";\"{1}\";\"{2}\"",
+                                     "Juanito", "Consulta a tutor", "HORA"));
+
+            System.Web.HttpContext.Current.Response.Write(sw.ToString());
+            System.Web.HttpContext.Current.Response.Flush();
+            System.Web.HttpContext.Current.Response.End();
+
+        }
         public CSV()
         {
         }
