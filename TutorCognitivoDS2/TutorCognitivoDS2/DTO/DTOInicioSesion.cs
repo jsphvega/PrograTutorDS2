1bdc1f9ed8f07296bbd7264f5e622843796b21fa
mariquita5 <majomadrig5@gmail.com>
2016-11-13 22:47:42 -0600

    +CAMBIOS

--- TutorCognitivoDS2/TutorCognitivoDS2/DTO/DTOInicioSesion.cs
+++ TutorCognitivoDS2/TutorCognitivoDS2/DTO/DTOInicioSesion.cs
@@ -7,5 +7,24 @@ namespace TutorCognitivoDS2.DTO
 {
     public class DTOInicioSesion
     {
+        private String correo;
+        private String contraseña;
+
+        public DTOInicioSesion(String pCorreo, String pContraseña)
+        {
+            correo = pCorreo;
+            contraseña = pContraseña;
+        }
+        public string Correo
+        {
+            get { return correo; }
+            set { correo = value; }
+        }
+
+        public string Contraseña
+        {
+            get { return contraseña; }
+            set { contraseña = value; }
+        }
     }
 }
