9bfbf016e92bd3eabdff94df7f6dc3ddd4da5704
mariquita5 <majomadrig5@gmail.com>
2016-11-15 16:02:38 -0600

    curso listo

--- TutorCognitivoDS2/TutorCognitivoDS2/TutorCognitivo/logicaDeNegocios/Curso.cs
+++ TutorCognitivoDS2/TutorCognitivoDS2/TutorCognitivo/logicaDeNegocios/Curso.cs
@@ -9,31 +9,38 @@ namespace logicaDeNegocios
         private String nombre;
         private ArrayList categorias;
 
-        public Curso(int pCodigo, String pNombre)
+        public Curso(String  pCodigo, String pNombre)
         {
+            this.codigo = pCodigo;
+            this.nombre = pNombre;
         }
 
         public void setCodigo(String pCodigo)
         {
+            this.codigo = pCodigo;
         }
 
         public String getCodigo()
         {
-            return null;
+            return codigo;
         }
 
         public void setNombre(String pNombre)
         {
+            this.nombre = pNombre;
         }
 
         public String getNombre()
         {
-            return null;
+            return nombre;
         }
 
         public String toString()
         {
-            return null;
+            String datos;
+            datos = "codigo" + codigo;
+            datos += "nombre" + nombre;
+            return datos;
         }
     }
 }
