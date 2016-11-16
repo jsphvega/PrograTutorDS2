e2853683e3849949ccfbbe97e076840dbec7b358
mariquita5 <majomadrig5@gmail.com>
2016-11-15 19:50:57 -0600

    +cambios

--- TutorCognitivoDS2/TutorCognitivoDS2/TutorCognitivo/controlador/Controlador.cs
+++ TutorCognitivoDS2/TutorCognitivoDS2/TutorCognitivo/controlador/Controlador.cs
@@ -4,11 +4,14 @@ using System;
 using System.Collections.Generic;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TutorCognitivoDS2.TutorCognitivo.controlador;
 
 namespace controlador
 {
     public class Controlador
+
     {
+        private Adaptador adaptador; 
         public List<String[]> obtenerListaCarreras()
         {
             DatoCarrera sCarrera = new DatoCarrera();
