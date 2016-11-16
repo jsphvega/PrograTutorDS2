c937f16e2d64f4facb254aa2e8c8d3939deee82b
krlaguilar02 <ka_yi_la1502@hotmail.es>
2016-11-15 22:12:12 -0600

    adaptador?

--- TutorCognitivoDS2/TutorCognitivoDS2/TutorCognitivo/controlador/Adaptador.cs
+++ TutorCognitivoDS2/TutorCognitivoDS2/TutorCognitivo/controlador/Adaptador.cs
@@ -5,8 +5,24 @@ using System.Web;
 
 namespace TutorCognitivoDS2.TutorCognitivo.controlador
 {
-    public class Adaptador:IAdaptador
+    public class Adaptador : IAdaptador
     {
-     
+        public void generarDocumento(String pTipo, ArrayList<Consulta> pConsultas)
+        {
+
+        }
+        private Consulta traducirRespuestas(Consulta pConsulta)
+        {
+
+        }
+        private void agregarInformacion(DTOEntrenamiento pEntrenamiento)
+        {
+
+        }
+        public ArrayList<DTOEntrenamiento> obtenerRespuestas(String pConsulta)
+        {
+
+        }
+
     }
 }
